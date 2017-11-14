bfabc11bf87d03ebbb6bd773d2a33d7182f5ba03
Uladzislau Sasnouski <mentalhook@gmail.com>
2017-11-14 16:40:37 -0500

    Add heatmap

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
-import Map from "./map.js";
+import h337 from 'heatmap.js';
+import * as firebase from "firebase";
+import { initializeFB } from './base.js';
+import * as deaths from './data.json';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
-  canvas.width = 1024;
-  canvas.height = 1024;
-  const map = new Map(canvas, ctx, canvas.width, canvas.height);
-  map.render();
+  initializeFB();
+  firebase.database().ref('/deaths').push(deaths);
+
+  const heatmapConfig = {
+    container: document.getElementById('heatmap'),
+    radius: 13,
+    maxOpacity: .5,
+    minOpacity: 0,
+    blur: .75
+  };
+
+  const deathData = [];
+  console.log(deaths);
+  for (let entry in deaths) {
+    const {x, y} = deaths[entry].location.victim;
+
+    let xPos = Math.floor(Math.abs(x - (-2203)) / 3764 * 840 + 64.7);
+    let yPos = Math.floor(969.7 - Math.abs((y - (-1031)) / 4090 * 923.7));
+
+    deathData.push({ x: xPos, y: yPos, value: 10 });
+  }
+
+  // create heatmap with configuration
+  const heatmapInstance = h337.create(heatmapConfig);
+
+  const data = {
+    max: 10,
+    data: deathData
+  }
+  //
+  heatmapInstance.setData(data);
 });
