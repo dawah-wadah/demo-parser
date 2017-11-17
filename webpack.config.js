7198f3eabff52b98d8661cc6880ffaccfd384edd
Uladzislau Sasnouski <mentalhook@gmail.com>
2017-11-17 16:52:32 -0500

    Update webpack to work with jsx files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,21 @@
 module.exports = {
-  entry: "./main.js",
+  context: __dirname,
+  entry: "./main.jsx",
   output: { filename: "./vlad.js" },
-  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
 };
