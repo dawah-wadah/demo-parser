6d01149ff1e322f578d9cc7a36204a3943b2f257
Uladzislau Sasnouski <mentalhook@gmail.com>
2017-11-26 15:12:52 -0500

    Accuracy data for each weapon is gathered

--- reader.js
+++ reader.js
@@ -65,6 +65,8 @@ function storeData(attacker, victim, status, map) {
 }
 
 function storeShots(playerName, weaponsData) {
+  let promises = [];
+
   Object.keys(weaponsData).forEach(weapon => {
     let data = {
       totalShots: weaponsData[weapon].shots_fired,
@@ -75,13 +77,20 @@ function storeShots(playerName, weaponsData) {
         100
       ).toFixed(2)
     };
-    console.log(weapon);
-    new Promise(function(resolve, reject) {
-      firebase
-        .database()
-        .ref(`/${playerName}/Weapons Data/${weapon}`)
-        .push(data);
-    });
+
+    promises.push(
+      new Promise(function(resolve, reject) {
+        firebase
+          .database()
+          .ref(`/${playerName}/Weapons Data/${weapon}`)
+          .push(data)
+          .then(() => resolve(), () => reject());
+      })
+    );
+  });
+
+  return Promise.all(promises).then(() => {
+    return;
   });
 }
 
@@ -189,7 +198,7 @@ function parseDemofile(file, callback) {
 
     demoFile.on("end", () => {
       // updateProgress(bar, demoFile);
-      // console.log("Finished with " + file);
+      console.log("Finished with " + file);
       Promise.all([
         storeShots("Taylor Swift", shots.wadah),
         storeShots("hlebopek", shots.vlad)
@@ -279,9 +288,10 @@ function parseDemofile(file, callback) {
           shots.wadah[e.weapon].headshots++;
         }
       } else if (playa.steam64Id == 76561198171618625) {
-        if (!shots.vlad[e.weapon].shots_hit) {
-          shots.vlad[e.weapon].shots_hit = newWeapon();
+        if (!shots.vlad[e.weapon]) {
+          shots.vlad[e.weapon] = newWeapon();
         }
+        shots.vlad[e.weapon].shots_hit++;
         if (e.hitgroup === 1 && e.health === 0) {
           shots.vlad[e.weapon].headshots++;
         }
