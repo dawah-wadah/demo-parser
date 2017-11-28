75422e6b08bee9cbf3a38b5cf65605c8dc89f518
Uladzislau Sasnouski <mentalhook@gmail.com>
2017-11-27 19:13:55 -0500

    Iitial bubbles are rendered

--- frontend/data.jsx
+++ frontend/data.jsx
@@ -6,10 +6,12 @@ export default class Data extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+
+    this.createChart = this.createChart.bind(this);
   }
 
   getShit(args) {
-    let data = [];
+    let data = {};
     Object.keys(args).forEach(weapon => {
       let weaponData = { [weapon]: { fired: 0, hits: 0, headshots: 0 } };
       console.log(weapon);
@@ -18,10 +20,11 @@ export default class Data extends React.Component {
         weaponData[weapon].hits += args[weapon][key].totalHits;
         weaponData[weapon].headshots += args[weapon][key].headshots;
       });
-      data.push(weaponData);
+      // data.push(weaponData);
+      data[weapon] = weaponData[weapon];
       // data.push({ [weapon]: args[weapon] });
     });
-    console.log(data);
+    this.setState({ weaponData: data });
     // data;
   }
 
@@ -31,31 +34,41 @@ export default class Data extends React.Component {
       .ref("/Taylor Swift/Weapons Data")
       .once("value", snapshot => {
         this.getShit(snapshot.val());
-        // console.log(snapshot.val());
-      });
+      })
+      .then(() => this.createChart());
+  }
+
+  adjustRadius(weapon) {
+    const { weaponData } = this.state;
+    const accuracy = (weaponData[weapon].hits / weaponData[weapon].fired * 100) + 20;
+
+    if (accuracy < 25) {
+      return 25;
+    } else if (accuracy > 80) {
+      return 80;
+    }
+
+    return accuracy;
+  }
+
+  createChart() {
+    const svg = d3.select(this.node);
+    const element = svg.selectAll("g").data(Object.keys(this.state.weaponData));
+    const elemEnter = element.enter()
+        .append("g")
+        .attr("transform", d => `translate(${Math.random() * 900 + 1}, ${Math.random() * 900 + 1})`);
+    console.log(this.state.weaponData);
+    const circle = elemEnter.append("circle")
+        .attr("r", d => this.adjustRadius(d))
+        .attr("fill", "pink");
+
+    elemEnter.append("text")
+      .attr("dx", d => -20)
+      .text(d);
   }
 
   render() {
-    let svg = d3.select("svg").attr("width", 950);
-    console.log(svg);
-
-    // // let width = +svg.attr("width");
-    // // let height = svg.attr("height");
-    // // console.log(width);
-    // //   height = svg.attr("height");
-    // //
-    // console.log(svg);
-    //   d3.select("body")
-    // .append("svg")
-    //   .attr("width", 960)
-    //   .attr("height", 500)
-    // .append("g")
-    //   .attr("transform", "translate(20,20)")
-    // .append("rect")
-    //   .attr("width", 920)
-    //   .attr("height", 460);
-    // let format = d3.format(",d");
-    // return <svg width="960" height="960" />;
-    return <h1>Wadah is Retarded</h1>;
+    return <svg ref={node => this.node = node} width="960" height="960" />;
+    // return (<h1>Wadah is Retarded</h1>);
   }
 }
