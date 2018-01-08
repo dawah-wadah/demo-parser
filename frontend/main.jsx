d7f4c498d270452d2164b5dc4faa1d3884fdcdbe
Wadah Adlan <wadahadlan@gmail.com>
2018-01-07 20:28:56 -0500

    removed unnessarary imports

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,18 +1,12 @@
-import Heatmap from "./heat";
-import Data from "./data";
-import KDChart from "./kd_chart";
-import Body from "./body";
 import Header from "./header";
 import Mainpage from "./mainpage";
 import Footer from "./footer";
-import Foo from './foo.jsx'
-import initializeFB from "./base.js";
+import initializeFB from "../base.js";
 import Player from './playerPage.jsx'
 
 import React from "react";
 import ReactDOM from "react-dom";
 import { Switch, Route, HashRouter } from "react-router-dom";
-import WeaponsChart from "./frontend/weapons_chart";
 
 initializeFB();
 document.addEventListener("DOMContentLoaded", () => {
@@ -29,10 +23,6 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" component={Mainpage} />
             <Route path="/players/:id" component={Player} />
-            <Route exact path="/player/:id/kd" component={KDChart} />
-            <Route exact path="/body" component={Body} />
-            <Route exact path="/foo" component={Foo} />
-            <Route exact path="/bar" component={WeaponsChart} />
           </Switch>
           <Footer />
         </div>
