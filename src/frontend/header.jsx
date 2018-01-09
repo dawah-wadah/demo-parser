71ea5d24fcb1aeb28171ffab9db36c86d15fa82d
Wadah Adlan <wadahadlan@gmail.com>
2018-01-09 12:56:26 -0500

    Title Change

--- src/frontend/header.jsx
+++ src/frontend/header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Header = () => (
   <header id="header">
     <h1>
-      <Link to="/">CS:GO Stats</Link>
+      <Link to="/">Counter-Stats: GO</Link>
     </h1>
   </header>
 );
