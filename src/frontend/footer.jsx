cc5642dc278759c7270c07751f46f49865dd7497
Uladzislau Sasnouski <mentalhook@gmail.com>
2018-01-13 16:36:36 -0500

    Finalize footer

--- src/frontend/footer.jsx
+++ src/frontend/footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Footer = () => (
   <footer id="footer">
     <h2>
-      This was made by <Link to="/creators">Wadah & Vlad</Link>
+      Made by <Link to="/creators">Wadah & Vlad</Link>
     </h2>
   </footer>
 );
