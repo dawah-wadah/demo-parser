3e9ad330d5dbe0f79a25dd6f7e8516512638b3b9
unknown <mentalhook@gmail.com>
2018-02-12 00:29:02 -0500

    Add color effect to header when mouse is over

--- src/frontend/header/header.jsx
+++ src/frontend/header/header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Header = () => (
   <header id="header">
     <h1>
-      <Link to="/">Counter-Stats: GO</Link>
+      <Link to="/"><span>Counter</span>-<span>Stats: GO</span></Link>
     </h1>
   </header>
 );
